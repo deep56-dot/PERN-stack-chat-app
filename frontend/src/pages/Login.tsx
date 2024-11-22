3a14d9c13336d7c72863ce6358a49f57eadc36c5
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-11-22 16:33:45 +0530

    authentication functinality implemented in frontend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+	const [inputs, setInputs] = useState({
+		username: "",
+		password: ""
+	});
+
+	const {login,loading}=useLogin()
+
+	const handleSubmitForm = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log(inputs)
+		login(inputs.username,inputs.password)
+	};
+
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -9,12 +24,14 @@ const Login = () => {
 					<span className='text-blue-500'> ChatApp</span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmitForm}>
 					<div>
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' 
+						value={inputs.username} 
+						onChange={(e)=>setInputs({...inputs,username:e.target.value})}  />
 					</div>
 
 					<div>
@@ -25,7 +42,9 @@ const Login = () => {
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
-						/>
+							value={inputs.password} 
+						onChange={(e)=>setInputs({...inputs,password:e.target.value})}  />
+						
 					</div>
 					<Link
 						to='/signup'
@@ -35,7 +54,7 @@ const Login = () => {
 					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2'>Login</button>
+						<button className='btn btn-block btn-sm mt-2'>{loading?"Loading...":"Login"}</button>
 					</div>
 				</form>
 			</div>
